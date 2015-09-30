fe31a4f18d91e77db5c5266dacd6a140fd148e74
smart__petea <smart__petea@mail.ru>
2015-09-30 22:18:23 +0300

    install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
+apt-get install tmux
 git submodule init
 git submodule update
 echo 'alias tmux="TERM=screen-256color-bce tmux"' >> ~/.bashrc
 touch ~/.tmux.conf
 cat .tmux.conf >> ~/.tmux.conf
+exec bash
