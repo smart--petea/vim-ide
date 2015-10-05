81e0bfa6b2c11bb4efbe67e3ad5f8d71b2d39f79
petru <smart__petea@mail.ru>
2015-10-05 11:38:47 +0300

    install.sh

--- install.sh
+++ install.sh
@@ -4,14 +4,11 @@ apt-get install tmux
 git submodule init
 git submodule update
 
-echo 'alias tmux="TERM=screen-256color-bce tmux"' >> ~/.bashrc
-touch ~/.tmux.conf
-cat .tmux.conf >> ~/.tmux.conf
-
-exec bash
-
-rm .vimrc
+rm ~/.vimrc
 ln -s ~/.vim/.vimrc ~/.vimrc
 
-rm .tmux.conf
+rm ~/.tmux.conf
 ln -s ~/.vim/.tmux.conf ~/.tmux.conf
+
+echo 'alias tmux="TERM=screen-256color-bce tmux"' >> ~/.bashrc
+exec bash
