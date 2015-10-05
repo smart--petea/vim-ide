9cc46cda6d7840f5761b260ad7825739199c93e4
petru <smart__petea@mail.ru>
2015-10-05 11:23:09 +0300

    ln -s .vimrc

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 apt-get install exuberant-ctags
 apt-get install tmux
+
 git submodule init
 git submodule update
+
 echo 'alias tmux="TERM=screen-256color-bce tmux"' >> ~/.bashrc
 touch ~/.tmux.conf
 cat .tmux.conf >> ~/.tmux.conf
+
 exec bash
+
+ln -s ~/.vim/.vimrc ~/.vimrc
